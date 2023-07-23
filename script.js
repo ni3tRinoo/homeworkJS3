a6f879f749b4fea46995e0599c7bbfda25af68c6
ni3tRinoo <vladik07@yahoo.com>
2023-07-23 20:45:14 +0200

    First commit

---
+++ script.js
@@ -0,0 +1,99 @@
+// let message = "Одного"
+
+// console.log(message.startsWith("Одного"))
+
+// console.log(message.lenght);
+// console.log(message.padEnd(10, "!"))
+
+// let surname = "Ivanov";
+
+// let years = 14;
+
+// let name = "Vladislav";
+
+// let hello = "Hello, my name is" + name + surname + "i have" + years
+
+// let print = `Hello, my name is ${name} ${surname}, i have ${years} old`;
+
+// console.log(print);
+// console.log(hello);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// //1
+
+// let fullName = "Vladislav Ivanov";
+
+// console.log(fullName.length);
+
+// //2
+
+// let name = "vladislav";
+
+// console.log(name.toUpperCase());
+
+//3
+
+// let name = "Vladislav";
+
+// console.log(name.charAt(0))
+
+//4
+
+// const fullName = "Vladislav Ivanov";  
+
+// console.log(fullName.indexOf(" "))
+
+//5
+
+// let name = "Vladislav";
+
+// console.log(name.replace("V", "@"))
